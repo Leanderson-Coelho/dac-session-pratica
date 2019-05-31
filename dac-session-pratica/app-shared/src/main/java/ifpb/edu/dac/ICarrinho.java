ee228331dcd4439086368b3d5dfebe962cad0feb
Marciel Sousa <marcielmj@gmail.com>
2019-05-31 10:56:25 -0300

    Adicionado: metódo para calcular subtotal no git status

--- dac-session-pratica/app-shared/src/main/java/ifpb/edu/dac/ICarrinho.java
+++ dac-session-pratica/app-shared/src/main/java/ifpb/edu/dac/ICarrinho.java
@@ -1,11 +1,13 @@
 package ifpb.edu.dac;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public interface Carrinho<P, C> {
+public interface ICarrinho<P, C> {
     public Boolean addProduto (P p);
     public Boolean removeProduto (P p);
     public List<P> getProdutos ();
     public void setCliente (C c);
+    public BigDecimal getSubtotal ();
     public void checkout ();
 }
